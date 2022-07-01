c78d7ca710021830ad8a609a3b967081396ab48e
Вячеслав Киселёв <vskiseleff@yandex.ru>
2022-07-01 13:00:08 +0300

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener listenerError = System.out::println;
+
+        Worker worker = new Worker(listener, listenerError);
+        worker.start();
+    }
+}
